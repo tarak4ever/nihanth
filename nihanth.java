c52959719ccdd3191db738621180d90f96c50e20
tarak4ever <tarak4ever@gmail.com>
2025-03-27 16:43:20 +0000

    First commit

---
+++ nihanth.java
@@ -0,0 +1,8 @@
+
+An astronaut is someone who travels outside Earth into space, conducting scientific research and operating spacecraft
+
+Astronaut
+
+An astronaut is a person trained, equipped, and deployed by a human spaceflight program to serve as a commander or crew member aboard a spacecraft
+
+Nihanth
---
+++ thanavya.java
@@ -0,0 +1,7 @@
+
+DOCTOR
+A doctor, or physician, is a medical professional licensed to diagnose, treat, and prevent illnesses and injuries, offering essential care, prescribing medications, and performing surgeries. 
+Here's a more detailed look at what doctors do and some of the qualities they possess:
+
+A physician, medical practitioner, medical doctor, or simply doctor is a health professional who practices medicine, which is concerned with promoting, maintaining or restoring health through the study, diagnosis, prognosis and treatment of disease, injury, and other physical and mental impairments.
+
